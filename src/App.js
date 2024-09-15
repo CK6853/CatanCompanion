1d53626ba20b66d0fbecabb44243854d7fb57635
CK6853 <caleb.klein@hotmail.com>
2024-09-16 09:55:36 +1000

    Add some classes, clean up navigation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 function App() {
   // Set up state for page
   // Could use routes, but this is a toy application so cbf
-  const [currentPageState, setCurrentPageState] = useState("Names")
+  const [currentPageState, setCurrentPageState] = useState("Players")
 
   // Set up state for name array
   const [nameArray, setNameArray] = useState(() => {
@@ -26,11 +26,11 @@ function App() {
   }, [nameArray]);
 
   switch(currentPageState) {
-    case "Names":
-      return (<Names returnHome={() => setCurrentPageState("Home")} names={nameArray} setNames={setNameArray}/>)
+    case "Players":
+      return (<Players returnHome={() => setCurrentPageState("Home")} players={nameArray} setPlayers={setNameArray}/>)
 
     default: // Should be "Home", but catch-all just in case
-      return (<Home setCurrentPageState={setCurrentPageState} names={nameArray}/>)
+      return (<Home setCurrentPageState={setCurrentPageState} players={nameArray}/>)
   }
 }
 
@@ -38,54 +38,65 @@ function App() {
 function Home(props) {
   return (
     <div className="Home">
-      <button onClick={() => props.setCurrentPageState("Names")}>Go To Names</button>
-      <h1>List of names</h1>
-      {props.names.map((name) => (<p>{name}</p>))}
+      <div className="NavBar">
+        {["Players", "Settlements"].map((pageName) => (
+          <button className="NavButton" onClick={() => props.setCurrentPageState(pageName)}>{pageName}</button>
+        ))}
+      </div>
+      
+      <h1>List of players</h1>
+      {props.players.map((name) => (<p>{name}</p>))}
     </div>
   )
 }
 
-//Names Page
-function Names(props) {
+//Players Page
+function Players(props) {
   // State for name entry field
   const [currentEntry, setCurrentEntry] = useState("")
 
   // Functions for buttons to call
   // Use form to add a name to the array passed from App and clear the form
-  function addToNames(addName) {
+  function addToPlayers(addName) {
     if (!addName) return;
-    props.setNames([...props.names, addName])
+    props.setPlayers([...props.players, addName])
     setCurrentEntry("")
   }
 
-  // Clear the names from App
-  function clearNames() {
-    props.setNames([])
+  // Clear the players from App
+  function clearPlayers() {
+    props.setPlayers([])
   }
 
   return (
-    <div className="Names">
-      {/*Button to return home*/}
-      <button onClick={() => props.returnHome()}>Return Home</button>
-      <div />
-      {/*Input field to enter a new name to add to the list*/}
-      <input
-        type="text"
-        /*Use the state set above to hold value*/
-        value={currentEntry}
-        /*When updated, update the state*/
-        onChange={(event) => setCurrentEntry(event.target.value)}
-        /*If Enter is pressed, submit*/
-        onKeyDown={(event) => {if(event.key==="Enter"){addToNames(currentEntry)}}}
-        placeholder="Enter Name"
-      />
-      {/*Manual button click*/}
-      <button onClick={() => {addToNames(currentEntry)}}>Add</button>
+    <div className="Players">
+      {/*Nav to return home*/}
+      <div className="NavBar">
+        <button className="NavButton" onClick={() => props.returnHome()}>Return Home</button>
+      </div>
+      <div className="PlayersBody">
+        {/*Input field to enter a new name to add to the list*/}
+        <input
+          className="NameInput"
+          type="text"
+          /*Use the state set above to hold value*/
+          value={currentEntry}
+          /*When updated, update the state*/
+          onChange={(event) => setCurrentEntry(event.target.value)}
+          /*If Enter is pressed, submit*/
+          onKeyDown={(event) => {if(event.key==="Enter"){addToPlayers(currentEntry)}}}
+          placeholder="Enter Name"
+        />
+        {/*Manual button click*/}
+        <button className="NameSubmit" onClick={() => {addToPlayers(currentEntry)}}>Add</button>
+        <div/>
+
+        {/*Button to clear players*/}
+        <button className="NameClear" onClick={() => clearPlayers()}>Clear Players</button>
       <div/>
-      {/*Button to clear names*/}
-      <button onClick={() => clearNames()}>Clear Names</button>
-      <div/>
-      {props.names.map((name) => (<p>{name}</p>))}
+      </div>
+      
+      {props.players.map((name) => (<p>{name}</p>))}
       
     </div>
   )
