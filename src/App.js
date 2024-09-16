1fafe009581744ae30261dbac8ac446b2595a34a
CK6853 <caleb.klein@hotmail.com>
2024-09-16 10:36:37 +1000

    Set up state for settlements

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Players from "./Players.js"
 import Settlements from "./Settlements.js"
 
 function App() {
-  // Set up state for page
+  // Set up state for active page
   // Could use routes, but this is a toy application so cbf
   const [currentPageState, setCurrentPageState] = useState("Players")
 
@@ -22,10 +22,29 @@ function App() {
     }
   });
 
+  // Set up state for settlements array
+  const [settlementArray, setSettlementArray] = useState(() => {
+    // Check localStorage to see if this is already initialised
+    const saved = localStorage.getItem("settlementArray");
+    const initialValue = JSON.parse(saved);
+
+    // If it already exists, use that. Otherwise, default to empty array
+    if (initialValue) {
+      return initialValue
+    } else {
+      return []
+    }
+  });
+
   // Set up Effect to save player array to localStorage whenever it is updated
   useEffect(() => {
     localStorage.setItem("playerArray", JSON.stringify(playerArray));
   }, [playerArray]);
+
+  // Set up Effect to save settlement array to localStorage whenever it is updated
+  useEffect(() => {
+    localStorage.setItem("settlementArray", JSON.stringify(settlementArray));
+  }, [settlementArray]);
 
   // Function for pages to return home
   function returnHome() {
@@ -37,7 +56,7 @@ function App() {
       return (<Players returnHome={returnHome} players={playerArray} setPlayers={setPlayerArray}/>)
 
     case "Settlements":
-      return (<Settlements returnHome={returnHome} />)
+      return (<Settlements returnHome={returnHome} settlements={settlementArray} setSettlements={setSettlementArray} players={playerArray}/>)
 
     default: // Should be "Home", but catch-all just in case
       return (<Home setCurrentPageState={setCurrentPageState} players={playerArray}/>)
