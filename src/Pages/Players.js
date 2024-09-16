a69b4b98a5a75667b3c1636f3c4ce0a6fe17c419
CK6853 <caleb.klein@hotmail.com>
2024-09-16 14:15:47 +1000

    Refactor some components

--- src/Pages/Players.js
+++ src/Pages/Players.js
@@ -2,74 +2,82 @@ import { useState } from "react";
 
 //Players Page
 export default function Players(props) {
-    // State for player entry field
-    const [currentEntry, setCurrentEntry] = useState("")
-  
-    // Functions for buttons to call
-    // Use form to add a player to the array passed from App and clear the form
-    function addToPlayers(playerName) {
-      // Avoid adding from an empty field
-      if (!playerName) return
-      // Update the list
-      props.setPlayers([...props.players, playerName])
-      // Reset the field
-      setCurrentEntry("")
-    }
-  
-    // Remove a specific player
-    function removePlayer(playerName) {
-      // Use an array filter to set players state without modifying the state itself
-      props.setPlayers(props.players.filter((player) => player !== playerName))
-    }
-  
-    // Clear the players
-    function clearPlayers() {
-      props.setPlayers([])
-    }
-  
-    return (
-      <div className="Players">
-        {/*Nav to return home*/}
-        <div className="NavBar">
-          <button className="NavButton" onClick={() => props.returnHome()}>Return Home</button>
-        </div>
-        <h1>Players</h1>
-        <div className="Body">
-          <div className="InputBlock">
-            {/*Input field to enter a new player to add to the list*/}
-            <input
-              className="InputBox"
-              type="text"
-              /*Use the state set above to hold value*/
-              value={currentEntry}
-              /*When updated, update the state*/
-              onChange={(event) => setCurrentEntry(event.target.value)}
-              /*If Enter is pressed, submit*/
-              onKeyDown={(event) => {if(event.key==="Enter"){addToPlayers(currentEntry)}}}
-              placeholder="Enter Player Name"
-            />
-            {/*Manual button click*/}
-            <button className="SubmitButton" onClick={() => {addToPlayers(currentEntry)}}>Add</button>
-          </div>
-          {/*Block to display all current players for possible removal - only render if data exists*/}
-          {props.players.length === 0 ? (null) : (
-            <div className="EditBlock">
-              <table className="PlayersTable">
-                {props.players.map((player) => (
-                  <tr>
-                    <td>{player}</td>
-                    {/*Dynamically create remove buttons*/}
-                    <td><button className="RemoveButton" onClick={() => removePlayer(player)}>Remove</button></td>
-                  </tr>
-                ))}
-              </table>
-              {/*Button to clear all players - only render if more than one player*/}
-              {props.players.length < 2 ? (null) : (
-                <button className="ClearButton" onClick={() => clearPlayers()}>Clear All Players</button>
-              )}
-            </div>
-          )}
-        </div>
+  return (
+    <div className="Players">
+      {/*Nav to return home*/}
+      <div className="NavBar">
+        <button className="NavButton" onClick={() => props.returnHome()}>Return Home</button>
       </div>
-    )
+      <h1>Players</h1>
+      <div className="Body">
+        <PlayerInputBlock players={props.players} setPlayers={props.setPlayers}/>
+        {/*Block to display all current players for possible removal - only render if data exists*/}
+        {props.players.length === 0 ? (null) : (<PlayerEditBlock players={props.players} setPlayers={props.setPlayers}/>)}
+      </div>
+    </div>
+  )
+}
+
+function PlayerInputBlock(props) {
+  // State for player entry field
+  const [currentEntry, setCurrentEntry] = useState("")
+
+  // Use form to add a player to the array passed from App and clear the form
+  function addToPlayers(playerName) {
+    // Avoid adding from an empty field
+    if (!playerName) return
+    // Update the list
+    props.setPlayers([...props.players, playerName])
+    // Reset the field
+    setCurrentEntry("")
   }
+  return (
+    <div className="InputBlock">
+      {/*Input field to enter a new player to add to the list*/}
+      <input
+        className="InputBox"
+        type="text"
+        /*Use the state set above to hold value*/
+        value={currentEntry}
+        /*When updated, update the state*/
+        onChange={(event) => setCurrentEntry(event.target.value)}
+        /*If Enter is pressed, submit*/
+        onKeyDown={(event) => {if(event.key==="Enter"){addToPlayers(currentEntry)}}}
+        placeholder="Enter Player Name"
+      />
+      {/*Manual button click*/}
+      <button className="SubmitButton" onClick={() => {addToPlayers(currentEntry)}}>Add</button>
+    </div>
+  )
+}
+
+function PlayerEditBlock(props) {
+  // Remove a specific player
+  function removePlayer(playerName) {
+    // Use an array filter to set players state without modifying the state itself
+    props.setPlayers(props.players.filter((player) => player !== playerName))
+  }
+
+  // Clear the players
+  function clearPlayers() {
+    props.setPlayers([])
+  }
+
+  return (
+    <div className="EditBlock">
+      <table className="PlayersTable">
+        {props.players.map((player) => (
+          <tr>
+            <td>{player}</td>
+            {/*Dynamically create remove buttons*/}
+            <td><button className="RemoveButton" onClick={() => removePlayer(player)}>Remove</button></td>
+          </tr>
+        ))}
+      </table>
+      {/*Button to clear all players - only render if more than one player*/}
+      {props.players.length < 2 ? (null) : (
+        <button className="ClearButton" onClick={() => clearPlayers()}>Clear All Players</button>
+      )}
+    </div>
+  )
+}
