abff91d87596384be00a3ec5d7eefdf2c80a67ce
CK6853 <caleb.klein@hotmail.com>
2024-09-17 15:04:05 +1000

    Undid constant refactoring - turns out I didn't need them elsewhere

--- src/Pages/Settlements.js
+++ src/Pages/Settlements.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { allowableRolls, resourceList } from "../constants";
+
+export const resourceList = ["Gold", "Wood", "Stone", "Bricks", "Sheep", "Hay"]
+export const allowableRolls = [2,3,4,5,6, 8,9,10,11,12]
 
 //Settlements Page
 // Props needed: returnHome(), settlements[], setSettlements(), players[]
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const resourceList = ["Gold", "Wood", "Stone", "Bricks", "Sheep", "Hay"]
-export const allowableRolls = [2,3,4,5,6, 8,9,10,11,12]
