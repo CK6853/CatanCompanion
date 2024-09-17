c4830e83ad1b865a9da9b28616cd2dfd10479703
CK6853 <caleb.klein@hotmail.com>
2024-09-17 14:53:37 +1000

    Changed code whitespace to be consistent

--- src/Pages/Robber.js
+++ src/Pages/Robber.js
@@ -1,16 +1,16 @@
 // Robber Page
 // Props needed: returnHome(), settlements[], setSettlements(), players[]
 export default function Robber(props) {
-    return (
-      <div className="Robber">
-        {/*Nav to return home*/}
-        <div className="NavBar">
-          <button className="NavButton" onClick={() => props.returnHome()}>Return Home</button>
-        </div>
-        <h1>Robber</h1>
-        <div className="Body">
-          
-        </div>
+  return (
+    <div className="Robber">
+      {/*Nav to return home*/}
+      <div className="NavBar">
+        <button className="NavButton" onClick={() => props.returnHome()}>Return Home</button>
       </div>
-    )
-  }
+      <h1>Robber</h1>
+      <div className="Body">
+        
+      </div>
+    </div>
+  )
+}
