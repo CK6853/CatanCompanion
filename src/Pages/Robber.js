680bd2d061b5109f85d78290e2366c99877ed9e5
CK6853 <caleb.klein@hotmail.com>
2024-09-18 09:00:22 +1000

    Initial functionality

--- src/Pages/Robber.js
+++ src/Pages/Robber.js
@@ -1,14 +1,143 @@
+import { useState, useEffect } from "react";
+import ReactSelect from "../ReactSelect";
+import { allowableRolls, resourceList } from "../Constants";
+
 // Robber Page
 // Props needed: returnHome(), settlements[], setSettlements(), players[]
 export default function Robber(props) {
+  
+  // Re-enable any disabled settlements each time this page is loaded
+  // Should probably use a useEffect, but then it complains about missing dependancies even when I specifically want an empty dependancy array
+  // State is just an array of object pointers, so even shallow-copying and then editing mutates the state... so just do it directly
+  props.settlements.forEach((settlement) => settlement.enabled = true)
+  
+  /*useEffect(() => {
+    console.log("Effect")
+    props.settlements.forEach((settlement) => settlement.enabled = true)
+  }, [])*/
+
   return (
     <div className="Robber">
       <h1>Robber</h1>
-      <div className="Body">
-        
-      </div>
       {/*Button to return home once settlements have been disabled*/}
       <button className="ConfirmButton" onClick={() => props.returnHome()}>Done</button>
+      <div className="Body">
+        <SettlementTable settlements={props.settlements} players={props.players}/>
+      </div>
+      
     </div>
   )
+}
+
+function SettlementTable(props) {
+  // Set up states for filters
+  const [rollFilter, setRollFilter] = useState("Roll Filter")
+  const [playerFilter, setPlayerFilter] = useState("Player Filter")
+  const [resourceFilter, setResourceFilter] = useState("Resource Filter")
+
+  const [filteredSettlements, setFilteredSettlements] = useState(props.settlements)
+  const [disabledSettlements, setDisabledSettlements] = useState(props.settlements)
+
+  // UseEffect can't tell when a Settlement class method is run, so we'll just trigger it manually...
+  // Hate this, but can't see another way around it
+  const [reRender, setReRender] = useState(true)
+
+  // When filters or the data are updated, update the filtered data
+  useEffect(() => {
+    setFilteredSettlements(filterEnabledSettlements(props.settlements, rollFilter, playerFilter, resourceFilter))
+    setDisabledSettlements(props.settlements.filter((settlement) => {return settlement.enabled === false}))
+  }, [rollFilter, playerFilter, resourceFilter, props.settlements, reRender])
+
+
+  return (
+    <div className="RobberBlock">
+      {/*Table of all settlements, to allow removals*/}
+      <table className="RobberTable">
+        {/*Header row*/}
+        <thead>
+          <tr>
+            <th>Roll</th>
+            <th>Player</th>
+            <th>Resource</th>
+            <th>Type</th>
+            <th>Enabled</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*Disabled settlements shown separately*/}
+          <RobberTableSection settlements={disabledSettlements} reRender={() => setReRender(!reRender)} enabled={false}/>
+          {/*Filters*/}
+          <tr>
+            <td><ReactSelect options={["Roll Filter", ...allowableRolls]} updateValue={setRollFilter}/></td>
+            <td><ReactSelect options={["Player Filter", ...props.players]} updateValue={setPlayerFilter}/></td>
+            <td><ReactSelect options={["Resource Filter", ...resourceList]} updateValue={setResourceFilter}/></td>
+          </tr>
+          {/*Data rows - filtered*/}
+          {/*Check for over-filter - display error after table to preserve formatting*/}
+          {filteredSettlements.length === 0 ? null :
+          <RobberTableSection settlements={filteredSettlements} reRender={() => setReRender(!reRender)} enabled={true}/>}
+        </tbody>
+      </table>
+
+      {/*Display error if over-filtered*/}
+      {filteredSettlements.length === 0 ? <p>No settlements match your filters</p> : null}
+
+      <button onClick={() => console.log(props.settlements)}>PRINT</button>
+    </div>
+  )
+}
+
+function RobberTableSection(props) {
+  return props.settlements.map((settlement, index) => (<RobberTableEntry key={index} settlement={settlement} reRender={props.reRender} enabled={props.enabled}/>))
+}
+
+function RobberTableEntry(props) {
+
+  function toggleEnabled() {
+    props.settlement.toggleEnabled()
+    props.reRender()
+  }
+
+  return (
+    <tr>
+      <td>{props.settlement.roll}</td>
+      <td>{props.settlement.player}</td>
+      <td>{props.settlement.resource}</td>
+      <td>{props.settlement.type}</td>
+      <td><input type="checkbox" checked={props.enabled} onChange={() => toggleEnabled()} /></td>
+    </tr>
+  )
+}
+
+//{props.settlement.enabled ? "☑" : "☐"}
+
+// Get a filtered list of settlements based on given filters - not including disabled settlements
+function filterEnabledSettlements(settlementArray, rollFilter, playerFilter, resourceFilter) {
+  // Don't mutate state directly - arrays are just pointers
+  let shallowCopy = [...settlementArray]
+
+  // Filter out disabled settlements
+  shallowCopy = shallowCopy.filter((settlement) => {return settlement.enabled === true})
+
+  // Check if default (unfiltered) state
+  if (rollFilter !== "Roll Filter") {
+    // If not, apply filter
+    shallowCopy = shallowCopy.filter((settlement) => {
+      return settlement.roll === rollFilter
+    })
+  }
+  // repeat
+  if (playerFilter !== "Player Filter") {
+    shallowCopy = shallowCopy.filter((settlement) => {
+      return settlement.player === playerFilter
+    })
+  }
+
+  if (resourceFilter !== "Resource Filter") {
+    shallowCopy = shallowCopy.filter((settlement) => {
+      return settlement.resource === resourceFilter
+    })
+  }
+
+  return shallowCopy
 }
