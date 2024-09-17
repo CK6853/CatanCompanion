459bb8b0ee27318a2545c724769c7185a8919a1f
CK6853 <caleb.klein@hotmail.com>
2024-09-17 11:49:08 +1000

    ...you can't roll a 1 on two dice

--- src/Pages/Settlements.js
+++ src/Pages/Settlements.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const allowableRolls = [1,2,3,4,5,6, 8,9,10,11,12]
+const allowableRolls = [2,3,4,5,6, 8,9,10,11,12]
 const resourceList = ["Gold", "Wood", "Stone", "Bricks", "Sheep", "Hay"]
 
 //Settlements Page
